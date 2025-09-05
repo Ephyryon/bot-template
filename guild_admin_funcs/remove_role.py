eb7b4da70cb535e0ad8157543a3d0d432dfcfbff
JonatanVG <jonatangr@bfk.no>
2025-09-05 10:04:36 +0200

    Main6

--- guild_admin_funcs/remove_role.py
+++ guild_admin_funcs/remove_role.py
@@ -2,18 +2,17 @@ import discord
 from bot_managment.bot_setup import bot
 from guild_funcs.register_role_with_guild import register_role_with_guild, registered_guilds
 
-## Add_role: Adds roles that gain a permit to use the supabase discord bot commands. !!!ONLY USABLE BY THE GUILD/GROUP OWNER!!!
-async def add_role(ctx, role: discord.Role, name: str = ""):
+async def remove_role(ctx, role: discord.Role, name: str = ""):
     guild_id = str(ctx.guild.id)
     if bot.user.name == name:
         if guild_id not in registered_guilds:
             registered_guilds[guild_id] = {"role_perms": []}
         if role.id not in registered_guilds[guild_id]["role_perms"]:
-            registered_guilds[guild_id]["role_perms"].append(role.id)
-            register_role_with_guild()
-            await ctx.send(f"Added role {role.name} ({role.id}) to the list.")
+            await ctx.send(f"Did not remove [{role}] from role_perms as [{role}] is not present within role_perms.")
+            return
         else:
-            await ctx.send(f"Role {role.name} is already registered.")
+            registered_guilds[guild_id]["role_perms"].remove(role.id)
+            await ctx.send(f"Removed [{role}] from role_perms successfully.")
         print(registered_guilds)
     else:
         None

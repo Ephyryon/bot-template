f8cf8c81399973397263aae2e7d6536924a6b5fb
JonatanVG <jonatangr@bfk.no>
2025-09-05 08:44:20 +0200

    Main3

--- bot_managment/ping.py
+++ bot_managment/ping.py
@@ -5,7 +5,7 @@ import discord
 async def ping(ctx, name: str = ""):
     if bot.user.name == name:
         latency = round(bot.latency * 1000) # Converts the latency to ms.
-        embed = discord.Embed(leading="Pong!", description=f"Latency: {latency}ms", color=discord.Color.blurple())
+        embed = discord.Embed(title="Ping!", description=f"Latency: {latency}ms", color=discord.Color.blurple())
         await ctx.send(embed=embed, ephemeral=True) # Sends the embed to the user who executed the command.
     else:
         None
